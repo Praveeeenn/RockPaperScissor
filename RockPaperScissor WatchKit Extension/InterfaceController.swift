b366e761b2544c9375ab9d51a9d7a46a004a1f6c
Praveen <praveenislive@gmail.com>
2020-03-09 11:50:59 +0530

    Initial Commit

---
+++ RockPaperScissor WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RockPaperScissor WatchKit Extension
+//
+//  Created by Praveen on 09/03/20.
+//  Copyright © 2020 Praveen. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
